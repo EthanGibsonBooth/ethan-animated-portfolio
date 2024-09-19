2432fa65c5925e8482eeefec543335d02e39ca30
Ethan Gibson-Booth <21271186@students.latrobe.edu.au>
2024-09-19 19:43:43 +1000

    fixed hero image

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -74,7 +74,7 @@ const Hero = () => {
         Software Engineer and Developer
       </motion.div>
       <div className="imageContainer">
-        <img src="/hero.png" alt="" />
+        <img src="public/hero.png" alt="" />
       </div>
     </div>
   );
